65a7b91288b0f60e9c364ebadccf802310a4fce0
Austin Wilmoth <wilmotha@oregonstate.edu>
2019-02-16 14:49:13 -0800

    added a bit to about me, finished 404 page, add more divs

--- server.js
+++ server.js
@@ -42,6 +42,16 @@ app.get('/about-me', function (req, res, next) {
 
 app.use(express.static('public'));
 
+app.get('*', function (req, res, next) {
+    res.status(404);
+    res.render('404', {
+        title: '404',
+        home: 'not',
+        projects: 'not',
+        aboutme: 'not'
+    });
+});
+
 app.listen(port, function(err){
     console.log("== Server is listening on port", port);
 });
